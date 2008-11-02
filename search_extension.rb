37a0cde7ff1c06d9526a627528786190d1acb1c7
Edmundo Valle Neto <edmundo.vn@gmail.com>
2008-11-02 10:30:46 -0200

    Added support to pagination using find_by_sql and added a method to load gems.

--- search_extension.rb
+++ search_extension.rb
@@ -16,6 +16,30 @@ class SearchExtension < Spree::Extension
   end
 
   def activate
+    # Add pagination support for the find_by_sql method inside paginating_find plugin.
+    PaginatingFind::ClassMethods.class_eval do
+      def paginating_sql_find(count_query, query, options)
+
+        # The current page defaults to 1 when not passed.
+        options[:current] ||= "1"
+
+        count_query = sanitize_sql(count_query)
+        query = sanitize_sql(query)
+   
+        # execute the count query - need to know how many records we're looking at   
+        count = count_by_sql(count_query)
+   
+        PagingEnumerator.new(options[:page_size], count, false, options[:current], 1) do |page|
+          # calculate the right offset values for current page and page_size
+            offset = (options[:current].to_i - 1) * options[:page_size]
+            limit = options[:page_size]
+   
+            # run the actual query - Note: do not include LIMIT statement in your query          
+          find_by_sql(query + " LIMIT #{offset},#{limit}")
+        end
+      end
+    end
+
 #    ProductsController.class_eval do
 #      private
 #      
@@ -107,4 +131,7 @@ class SearchExtension < Spree::Extension
     # admin.tabs.remove "Search"
   end
 
+  def self.require_gems(config)
+    config.gem 'activerecord-tableless', :lib => 'tableless'
+  end
 end
