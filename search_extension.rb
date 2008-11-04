2126fd6d87b14c864b9c4a941488352deeb6dc5c
Edmundo Valle Neto <edmundo.vn@gmail.com>
2008-11-04 09:44:25 -0200

    Updated the extension information.

--- search_extension.rb
+++ search_extension.rb
@@ -2,9 +2,9 @@
 require_dependency 'application'
 
 class SearchExtension < Spree::Extension
-  version "1.0"
+  version "0.1"
   description "Search and sort extension for spree."
-  url "http://yourwebsite.com/search"
+  url "http://github.com/edmundo/spree-search/tree/master"
 
   # Testing route.
   define_routes do |map|
@@ -39,80 +39,6 @@ class SearchExtension < Spree::Extension
         end
       end
     end
-
-#    ProductsController.class_eval do
-#      private
-#      
-#      def collection
-#        if params[:order_by]
-#          order_by = params[:order_by]
-#          ["master_price", "name", "available_on"].include?(params[:order_by]) || order_by = "name"
-#        end
-#      
-#        order_by ||= "name"
-#        
-#        if params[:side]
-#          side = params[:side]
-#          ["asc", "desc"].include?(params[:side]) || side = "asc"
-#        end
-#      
-#        side ||= "asc"
-#    
-#        if params[:search]
-#          @search_param = "- #{:searching_by.l_with_args({ :search_term => params[:search] })}"
-#          @collection ||= Product.available.by_name(params[:search]).find(
-#            :all,
-#            :order => "#{order_by} #{side}",
-#            :page => {:start => 1, :size => 4, :current => params[:p]}, 
-#            :include => :images)
-#        else
-#          @search_param = ""
-#          @collection ||= Product.available.find(
-#            :all,
-#            :order => "#{order_by} #{side}",
-#            :page => {:start => 1, :size => 4, :current => params[:p]},
-#            :include => :images)
-#        end
-#      end
-#    end
-#
-#    TaxonsController.class_eval do
-#      private
-#      
-#      def load_data
-#        if params[:order_by]
-#          order_by = params[:order_by]
-#          ["master_price", "name", "available_on"].include?(params[:order_by]) || order_by = "name"
-#        end
-#      
-#        order_by ||= "name"
-#        
-#        if params[:side]
-#          side = params[:side]
-#          ["asc", "desc"].include?(params[:side]) || side = "asc"
-#        end
-#      
-#        side ||= "asc"
-#    
-#        if params[:search]
-#          @search_param = "- #{:searching_by.l_with_args({ :search_term => params[:search] })}"
-#          @products ||= object.products.available.by_name(params[:search]).find(
-#            :all,
-#            :order => "#{order_by} #{side}",
-#            :page => {:start => 1, :size => 4, :current => params[:p]},
-#            :include => :images)
-#        else
-#          @search_param = ""
-#          @products ||= object.products.available.find(
-#            :all,
-#            :order => "#{order_by} #{side}",
-#            :page => {:start => 1, :size => 4, :current => params[:p]},
-#            :include => :images)
-#        end
-#    
-#        @product_cols = 3
-#      end
-#    end
 
     # Add support for internationalization to this extension.
     Globalite.add_localization_source(File.join(RAILS_ROOT, 'vendor/extensions/search/lang/ui'))
